a0e8f31ab11d3ebedadfb486f891958623f52b24
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2022-07-12 10:28:30 +0200

    Add test data

--- OTVideoPlayer/__main__.py
+++ OTVideoPlayer/__main__.py
@@ -365,9 +365,7 @@ class FrameQuickTimeStamps(tk.LabelFrame):
 def main():
     # global app
     # Create a window and pass it to the Application object
-    WindowVideoPlayer(
-        video_path=r"C:\Users\Baerwolff\Testvideos\Validierungsmessung_Radeberg\raspberrypi_FR20_2020-02-20_12-00-00.mp4"
-    )
+    WindowVideoPlayer(video_path=r"tests\data\raspberrypi_FR20_2020-02-20_12-00-00.mp4")
 
 
 if __name__ == "__main__":
