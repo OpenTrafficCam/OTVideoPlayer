a2e847f36370b9134d86fc75165a2521de3ec983
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2022-07-22 00:42:01 +0200

    Remove inital path

--- OTVideoPlayer/__main__.py
+++ OTVideoPlayer/__main__.py
@@ -4,7 +4,7 @@ from window_video_player import WindowVideoPlayer
 def main():
     # global app
     # Create a window and pass it to the Application object
-    WindowVideoPlayer(video_path=r"tests\data\raspberrypi_FR20_2020-02-20_12-00-00.mp4")
+    WindowVideoPlayer()
 
 
 if __name__ == "__main__":
